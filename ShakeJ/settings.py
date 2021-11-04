68045437b65fbf8efe4d854106183a02b72618ef
yoichikido <ydkido@gmail.com>
2021-11-04 20:54:00 +0900

    Secret key to environment variable

--- ShakeJ/settings.py
+++ ShakeJ/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0ibjqvd6fa15ubgaazd8=e2+wkl=!9kr__v6@=6c501+30^z-d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
